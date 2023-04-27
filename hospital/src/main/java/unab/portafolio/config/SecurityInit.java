fea01f528c60f48b4f4ab9b0225a324b9597a76c
Mauricio Gutierrez Ulloa <121682144+Gronorf@users.noreply.github.com>
2023-04-27 08:21:12 -0400

    proyecto hospital
    se agrega el proyecto al github

---
+++ hospital/src/main/java/unab/portafolio/config/SecurityInit.java
@@ -0,0 +1,7 @@
+package unab.portafolio.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends WebSecurityConfigurerAdapter {
+}
